d1e47c74fefdb5cbc2aa24ce19f1fef4363797f3
wangxin <wangxin>
2021-08-11 19:02:17 +0800

    '力扣94题'

---
+++ let_94/inorderTraversal_test.go
@@ -0,0 +1,23 @@
+package let_94
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+func inorderTraversal(root *TreeNode) []int {
+	var result []int
+	current := root
+	for current != nil {
+
+	}
+}
