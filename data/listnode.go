45f39d1f4da15d507f4b1fd5d1ebdf5d5a73f3da
wangxin <wangxin>
2022-03-18 19:03:59 +0800

    update

--- data/listnode.go
+++ data/listnode.go
@@ -5,6 +5,41 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func NewListNode(val int) *ListNode {
+	return &ListNode{
+		Val:  val,
+	}
+}
+
+//头插法
+func (l *ListNode) HeadInsert(val int) *ListNode {
+	node := &ListNode{Val: val}
+	if l == nil {
+		return node
+	}
+	node.Next = l
+	l = node
+	return node
+}
+
+//翻转链表
+func (l *ListNode) ReverseList() *ListNode {
+	if l == nil || l.Next == nil {
+		return l
+	}
+	node1 := l
+	node2 := l.Next
+	for node2 != nil {
+		next := node2.Next
+		node2.Next = node1
+		node1 = node2
+		node2 = next
+	}
+	l.Next = nil
+	l = node1
+	return node1
+}
+
 func ListNodeBySlice(nums []int) *ListNode {
 	head := &ListNode{}
 	current := head
