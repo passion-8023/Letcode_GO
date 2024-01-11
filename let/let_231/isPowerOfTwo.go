3b0fd61d8ebf56ddd3ae3b069bd5d60520bfd72a
wangxin <wangxin@microdreams.com>
2024-01-11 19:16:00 +0800

    update

---
+++ let/let_231/isPowerOfTwo.go
@@ -0,0 +1,17 @@
+package let_231
+
+func isPowerOfTwo(n int) bool {
+	if n == 0 {
+		return false
+	}
+
+	if n == 1 {
+		return true
+	}
+
+	if n%2 != 0 {
+		return false
+	}
+
+	return isPowerOfTwo(n / 2)
+}
