9cfc21d8a54e5224ce90f9c41106f022c6deaa80
wangxin <wangxin@microdreams.com>
2024-01-15 10:54:03 +0800

    let 50

--- let/let_50/myPow.go
+++ let/let_50/myPow.go
@@ -1,7 +1,5 @@
 package let_50
 
-import "fmt"
-
 func myPow(x float64, n int) float64 {
 	if x == 0 {
 		return 0
@@ -11,10 +9,35 @@ func myPow(x float64, n int) float64 {
 		return 1
 	}
 
-	fmt.Println(x, n)
+	if n == 1 {
+		return x
+	}
+
 	if n < 0 {
-		return 1 / (x * myPow(x, n+1))
+		return 1 / (x * myPow(x, -n-1))
 	} else {
 		return x * myPow(x, n-1)
 	}
+}
+
+func myPow1(x float64, n int) float64 {
+	var callbackFunc func(x float64, n int) float64
+	callbackFunc = func(x float64, n int) float64 {
+		if n == 0 {
+			return 1
+		}
+
+		val := callbackFunc(x, n/2)
+
+		if n%2 == 0 {
+			return val * val
+		}
+		return x * val * val
+	}
+
+	if n < 0 {
+		return 1.0 / callbackFunc(x, -n)
+	}
+
+	return callbackFunc(x, n)
 }
